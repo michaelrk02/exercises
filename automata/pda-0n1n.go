6244e4f857471d85edfe6419cc5c2bf3a1e44e5f
Michael Krisnadhi <michaelkrisnadhi@gmail.com>
2020-07-20 20:07:43 +0700

    update repo

--- automata/pda-0n1n.go
+++ automata/pda-0n1n.go
@@ -2,65 +2,49 @@ package main
 
 import (
     "fmt"
+    "github.com/michaelrk02/automata"
 )
 
-type byteStack []byte
-
-func (s byteStack) top() byte {
-    return s[0]
-}
-
-func (s *byteStack) push(c byte) {
-    *s = append([]byte{c}, (*s)...)
-}
-
-func (s *byteStack) pop() {
-    *s = (*s)[1:len(*s)]
-}
-
-func verify(s string) bool {
-    stack := make(byteStack, 0)
-    state := 0
-    for _, input := range s {
-        if state == 0 {
-            if input == '0' {
-                if len(stack) == 0 {
-                    stack.push('A')
-                } else if stack.top() == 'A' {
-                    stack.push('A')
-                }
-            }
-            if input == '1' {
-                if len(stack) == 0 {
-                } else if stack.top() == 'A' {
-                    stack.pop()
-                    state = 1
-                }
-            }
-            continue
-        }
-        if state == 1 {
-            if input == '0' {
-                stack.push('X')
-            }
-            if input == '1' {
-                if len(stack) == 0 {
-                    stack.push('X')
-                } else if stack.top() == 'A' {
-                    stack.pop()
-                }
-            }
-            continue
-        }
-    }
-    return state == 1 && len(stack) == 0
-}
-
 func main() {
-    var str string
-    fmt.Printf("Enter input string: ")
-    fmt.Scanf("%s", &str)
+    pda := automata.NewPushDownAutomata(4, 0, 2)
 
-    fmt.Printf("Result: %v\n", verify(str))
+    pda.States[0] = func(token, top byte) (int, automata.PDAStackOp) {
+        if (token == '0') && ((top == 0x00) || (top == 'A')) {
+            return 0, &automata.PDAStackOpPush{Symbol: 'A'}
+        }
+        if (token == '1') && (top == 'A') {
+            return 1, &automata.PDAStackOpIgnore{}
+        }
+        return 3, &automata.PDAStackOpIgnore{}
+    }
+
+    pda.States[1] = func(token, top byte) (int, automata.PDAStackOp) {
+        if (token == '1') && (top == 'A') {
+            return 1, &automata.PDAStackOpPop{}
+        }
+        if (token == 0x00) && (top == 'A') {
+            return 2, &automata.PDAStackOpPop{}
+        }
+        return 3, &automata.PDAStackOpIgnore{}
+    }
+
+    pda.States[2] = func(token, top byte) (int, automata.PDAStackOp) {
+        if (token == '1') {
+            return 3, &automata.PDAStackOpIgnore{}
+        }
+        return 2, &automata.PDAStackOpIgnore{}
+    }
+
+    pda.States[3] = func(token, top byte) (int, automata.PDAStackOp) {
+        return 3, &automata.PDAStackOpIgnore{}
+    }
+
+    for true {
+        var in string
+        fmt.Printf("input: ")
+        fmt.Scanf("%s", &in)
+
+        fmt.Printf("Accepted: %v\n", pda.Accepts(in))
+    }
 }
 
